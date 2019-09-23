03dda8e6b34f8c10db34b60b952e2af71d510d79
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2019-09-23 17:39:39 +0200

    lasta chance

--- api/routes.js
+++ api/routes.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const router = express.Router()
-
 let SiteController = require('./controllers/SiteController');
 let GithubController = require('./controllers/GithubController');
-
 // Site routes
 router.get('/', SiteController.index);
 // Github routes
