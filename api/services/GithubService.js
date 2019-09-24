7f096d50c79a382f9f91382bc584f2695e8904bf
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2019-09-24 18:10:19 +0200

    moved logic to services (as it should be)

--- api/services/GithubService.js
+++ api/services/GithubService.js
@@ -1,3 +1,43 @@
+const request = require('request-promise');
+
 module.exports = {
-    
+    getCommitsByWord: (word)=>{
+        let options = {
+            uri: `https://api.github.com/search/commits?q=repo/${word}`,
+            headers: {
+                'User-Agent': 'request',
+                'Accept': 'application/vnd.github.cloak-preview'
+            },
+            json: true
+        };
+        let response = request(options)
+            .then(function (info) {
+                let data = info.items.reduce((arr, curr) => {
+                    if (curr.author !== null){
+                        let info = {
+                            author: {
+                                login: curr.author.login,
+                                avatar: curr.author.avatar_url
+                            },
+                            commit: {
+                                message: curr.commit.message,
+                                url: curr.commit.url,
+                                date: curr.commit.committer.date
+                            },
+                            repo: {
+                                name: curr.repository.name,
+                                description: curr.repository.description
+                            }
+                        }
+                        arr.push(info);
+                    }
+                    return arr;
+                }, []);
+                return data;
+            })
+            .catch(function (err) {
+                return err;
+            });;
+        return response;
+    }
 }
