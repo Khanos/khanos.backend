e80f3c116ac4696e0b8af8b7d2b2fd15269965a5
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2019-09-25 20:48:46 +0200

    more apis, more test

--- api/services/GithubService.js
+++ api/services/GithubService.js
@@ -3,10 +3,63 @@ const request = require('request-promise');
 module.exports = {
     searchCommitsWithWord: (word)=>{
         let options = {
-            uri: process.env.GITHUB_SEARCH_COMMITS_URL+word,
+            uri: process.env.GITHUB_API_URL+`search/commits?q=repo/${word}`,
             headers: {
-                'User-Agent': 'request',
+                'User-Agent': 'khanos super api',
                 'Accept': 'application/vnd.github.cloak-preview'
+            },
+            json: true
+        };
+        let response = request(options)
+            .then(function (info) {
+                return info;
+            })
+            .catch(function (err) {
+                return err;
+            });;
+        return response;
+    },
+    getCommitsByRepoAndOwner: (owner, repoName) => {
+        let options = {
+            uri: process.env.GITHUB_API_URL+`repos/${owner}/${repoName}/commits`,
+            headers: {
+                'User-Agent': 'khanos super api'
+            },
+            json: true
+        };
+        let response = request(options)
+            .then(function (info) {
+                return info;
+            })
+            .catch(function (err) {
+                return err;
+            });;
+        return response;
+    },
+    getReposByWord: (word) => {
+        let options = {
+            uri: process.env.GITHUB_API_URL+`search/repositories?q=${repoName}+language:javascript&sort=stars&order=desc`,
+            headers: {
+                'User-Agent': 'khanos super api',
+                'Accept': 'application/vnd.github.mercy-preview+json'
+            },
+            json: true
+        };
+        let response = request(options)
+            .then(function (info) {
+                return info;
+            })
+            .catch(function (err) {
+                return err;
+            });;
+        return response;
+    },
+    getReposByWordAndLanguage: (word, language) => {
+        let options = {
+            uri: process.env.GITHUB_API_URL+`search/repositories?q=${repoName}+language:${repoName}&sort=stars&order=desc`,
+            headers: {
+                'User-Agent': 'khanos super api',
+                'Accept': 'application/vnd.github.mercy-preview+json'
             },
             json: true
         };
--- test/services/github.test.js
+++ test/services/github.test.js
@@ -1,20 +1,38 @@
 const GithubService = require('../../api/services/GithubService');
 const chai = require('chai');
 chai.should();
+chai.use(require('chai-things'));
 
 describe('GithubService', () => {
-    console.log('test in service');
-	describe('#searchCommitsWithWord()', () => {
-        console.log('debuggin')
+    
+    describe('#searchCommitsWithWord()', () => {
 		it('Should return an object with property: total_count, incomplete_results and items', (done) => {
             GithubService.searchCommitsWithWord('fuck')
             .then((commits) => {
-                console.log('debuggin more');
                 commits.should.be.a("object");
                 commits.should.have.property('total_count');
                 commits.should.have.property('incomplete_results');
                 commits.should.have.property('items');
                 commits.items.should.be.a('array');
+                done();
+            })
+            .catch((err) => {
+                done(err);
+            });
+        });
+    });
+    
+    describe('#getCommitsByRepoAndOwner()', () => {
+		it('Should return an object with property: sha, node_id, commit, url, comments_url and author', (done) => {
+            GithubService.getCommitsByRepoAndOwner('khanos', 'khanos.backend')
+            .then((commits) => {
+                commits.should.be.a("array");
+                commits.should.include.something.that.have.property('sha');
+                commits.should.include.something.that.have.property('node_id');
+                commits.should.include.something.that.have.property('commit');
+                commits.should.include.something.that.have.property('url');
+                commits.should.include.something.that.have.property('comments_url');
+                commits.should.include.something.that.have.property('author');
                 done();
             })
             .catch((err) => {
