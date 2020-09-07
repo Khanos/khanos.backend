112c58df67a95f5fcb63e7d4654d4dee54fa25a9
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2020-09-07 08:09:02 -0400

    adding more fields

--- api/controllers/GithubController.js
+++ api/controllers/GithubController.js
@@ -9,6 +9,8 @@ module.exports = {
             if(!data.error){
                 commits = data.items.map((item) => {
                     let info = {
+                        id: item.node_id,
+                        queryWord: word,
                         author: {
                             login: item.author ? item.author.login : item.commit.author.name,
                             avatar: item.author ? item.author.avatar_url : `https://www.fillmurray.com/${Math.floor(Math.random() * 200) + 200}/${Math.floor(Math.random() * 200) + 200}`
