1625164c4a43e0fa583009e04fb078a1f79cce1c
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2023-01-21 08:51:04 -0400

    removing language translation

--- api/controllers/OpenAiController.js
+++ api/controllers/OpenAiController.js
@@ -3,33 +3,19 @@ const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
-const translate = require('translatte');
 
 module.exports ={
     getResponse: async (req, res) => {
         const text = req.params.text;
         try {
-            const translatedText = await translate(text, {to: 'en'});
-            if(!translatedText.text) {
-                throw new Error('Error translating text');
-            }
             const completion = await openai.createCompletion({
                 model: "text-davinci-003",
-                prompt: `${translatedText.text}`,
+                prompt: `${text}`,
                 temperature: 0,
                 max_tokens: 100,
             });
-            let translatedResponse;
-            if(translatedText.from.language.iso !== 'en') {
-                translatedResponse = await translate(completion.data.choices[0].text, {to: translatedText.from.language.iso});
-                if(!translatedResponse.text) {
-                    throw new Error('Error translating response');
-                }
-            } else {
-                translatedResponse = completion.data.choices[0].text;
-            }
             return res.json({
-                text: translatedResponse,
+                text: completion.data.choices[0].text,
                 data: completion.data,
                 message: 'Success',
                 error: false,
