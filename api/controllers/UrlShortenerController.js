5894e1128aade1dde2969691a23540785c167842
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2024-03-07 10:58:09 -0400

    Adding tests

--- api/controllers/UrlShortenerController.js
+++ api/controllers/UrlShortenerController.js
@@ -1,6 +1,64 @@
+const UrlShortenerService = require('../services/UrlShortenerService');
+const { validateUrl } = require('../utils');
+const { getUrlList, getUrl, getShortUrl, deleteShortUrl, createNewShortUrl } = UrlShortenerService;
+
 const UrlShortenerController = {
-  index: (req, res) => {
-    res.send('Hello World!');
+  index: async (req, res) => {
+    try {
+      const urls = await getUrlList();
+      res.json(urls);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: error.message });
+    }
+  },
+  create: async (req, res) => {
+    try {
+      const { original_url } = req.body;
+      const hostname = validateUrl(original_url);
+      if (!hostname) {
+        throw new Error('Invalid URL');
+      }
+      const url = await getShortUrl(original_url);
+      if(!url.error) {
+        res.json(url.data);
+        return;
+      }
+      const newUrl = await createNewShortUrl(original_url);
+      if(newUrl.error) {
+        throw new Error(newUrl.message);
+      }
+      res.json(newUrl.data);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: error.message });
+    }
+  },
+  delete: async (req, res) => {
+    try {
+      const { short_url } = req.params;
+      const url = await deleteShortUrl(short_url);
+      if(url.error) {
+        throw new Error(url.message);
+      }
+      res.json(url.data);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: error.message });
+    }
+  },
+  getUrl: async (req, res) => {
+    try {
+      const { short_url } = req.params;
+      const url = await getUrl(short_url);
+      if(url.error) {
+        throw new Error(url.message);
+      }
+      res.json(url.data);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: error.message });
+    }
   }
 };
 
--- api/utils/index.js
+++ api/utils/index.js
@@ -1,14 +1,16 @@
 // Utils and stuff
 
 const hashCode = (url) => {
-  var hash = 0, i, chr;
-  if (url.length === 0) return hash;
-  for (i = 0; i < url.length; i++) {
-      chr   = url.charCodeAt(i);
-      hash  = ((hash << 5) - hash) + chr;
-      hash |= 0; // Convert to 32bit integer
+  let hash = 0;
+  if (url.length === 0) {
+    return hash;
   }
-  return Math.abs(hash);
+  for (let i = 0; i < url.length; i++) {
+    let char = url.charCodeAt(i);
+    hash = ((hash<<5)-hash)+char;
+    hash = hash & hash; // Convert to 32bit integer
+  }
+  return Math.abs(hash % 10000);
 };
 
 const validateUrl = (original_url) => {
